76fa60d4f3895f9061dbc99d762accd5833f9a23
toadless <thomasmcdonald2720@gmail.com>
2022-01-11 17:16:47 +0000

    oops

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express"
 import http from "http"
 import apollo from "./config/apollo.js"
 import middleware from "./config/middleware.js"
+import { addUser } from "./lib/auth.js" 
 import "./config/mongodb.js"
 
 // Initiating server - default express configuration
@@ -11,6 +12,9 @@ const app = express()
 
 // Load our middleware
 app.use(middleware);
+
+// Setting req.user
+app.use(addUser)
 
 apollo.applyMiddleware({ app, cors: false })
 // Build server using https or http depending on configurations declared above
